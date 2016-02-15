7efc14d750e7367166abfb6057872a9390bb2027
andlon93 <andreas.lones@gmail.com>
2016-02-15 15:35:22 +0100

    begun working

--- OneMax.py
+++ OneMax.py
@@ -6,18 +6,29 @@ class individual:
 	phenotype = []
 
 	def __init__(self, n):
-		self.genotype = self.makeRandomInit(n)
+		self.genotype = self.make_Random_Genotype(n)
 
 
 	### Function that: initialise a random list of 0s and 1s of length n
 	#	Input:         int n: the length
-	#   Outout:        The list of length n.
-	def makeRandomInit(self, n):
+	#   Outout:        Genotype
+	def make_Random_Genotype(self, n):
 		genotype = []
 		for i in range(n):
 			genotype.append(rng.randrange(0,2))
 		return genotype
 
+	### Function that: make phenotype based on the genotype
+	#	Input:         
+	#   Outout:        Phenotype
+	def make_Phenotype(self):
+		pass
+
+	### Function that: Calculate the fitness of the indidual
+	#	Input:         phenotype
+	#   Outout:        Fitness value
+	def Fitness(self):
+		pass
 
 
 if __name__ == '__main__':
