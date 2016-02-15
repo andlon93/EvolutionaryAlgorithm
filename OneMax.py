6ef6598b563330c80ea6cb2c0812f02b154db948
andlon93 <andreas.lones@gmail.com>
2016-02-15 15:49:50 +0100

    pheno and genotype methods works

--- OneMax.py
+++ OneMax.py
@@ -1,12 +1,14 @@
 import random as rng
-
+#
 class individual:
 	fitness = 0
 	genotype = []
 	phenotype = []
 
-	def __init__(self, n):
+	### Constructor
+	def __init__(self, n, mutation_prob):
 		self.genotype = self.make_Random_Genotype(n)
+		self.phenotype = self.make_Phenotype(self.genotype, mutation_prob)
 
 
 	### Function that: initialise a random list of 0s and 1s of length n
@@ -21,16 +23,33 @@ class individual:
 	### Function that: make phenotype based on the genotype
 	#	Input:         
 	#   Outout:        Phenotype
-	def make_Phenotype(self):
-		pass
+	def make_Phenotype(self, geno, prob):
+		# The list of phenotype bits
+		pheno = []
+
+		# Iterate through all bits in the genotype
+		for bit in geno:
+
+			# mutate with probability prob
+			if rng.random() < prob:
+				if bit == 1: 
+					pheno.append(0)
+				else: 
+					pheno.append(1)
+			else:
+				pheno.append(bit)
+
+		# Return the phenotype
+		return pheno
 
 	### Function that: Calculate the fitness of the indidual
 	#	Input:         phenotype
 	#   Outout:        Fitness value
 	def Fitness(self):
 		pass
-
-
+#
+#
 if __name__ == '__main__':
-	individ = individual(20)
+	individ = individual(20, 0.05)
 	print(individ.genotype)
+	print(individ.phenotype)
