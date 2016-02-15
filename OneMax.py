a2615ff020f0223745d0d78704cff7c9d938a912
andlon93 <andreas.lones@gmail.com>
2016-02-15 15:54:26 +0100

    simple fitness calculation working

--- OneMax.py
+++ OneMax.py
@@ -1,7 +1,6 @@
 import random as rng
 #
 class individual:
-	fitness = 0
 	genotype = []
 	phenotype = []
 
@@ -9,7 +8,7 @@ class individual:
 	def __init__(self, n, mutation_prob):
 		self.genotype = self.make_Random_Genotype(n)
 		self.phenotype = self.make_Phenotype(self.genotype, mutation_prob)
-
+		self.fitness = self.Fitness()
 
 	### Function that: initialise a random list of 0s and 1s of length n
 	#	Input:         int n: the length
@@ -46,10 +45,15 @@ class individual:
 	#	Input:         phenotype
 	#   Outout:        Fitness value
 	def Fitness(self):
-		pass
+		fitness = 0
+		for bit in self.phenotype:
+			if bit == 1:
+				fitness += 1
+		return fitness
 #
 #
 if __name__ == '__main__':
 	individ = individual(20, 0.05)
 	print(individ.genotype)
 	print(individ.phenotype)
+	print(individ.fitness)
